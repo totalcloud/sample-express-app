97bd7681eea2c157b8c0519adef5e5e4a3f6682e
Shubham Prasad <shubham@totalcloud.io>
2021-04-07 13:46:34 +0530

    Adding installation scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name: "sample-web-app",
+    script: "./bin/www",
+    env: {
+      NODE_ENV: "development",
+    }
+  }],
+};
---
+++ scripts/after_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir /home/ubuntu/nodejs
+cd /home/ubuntu/nodejs
