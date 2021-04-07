6573bc655ceeb97f12f5ee37485e5b7e5f2981fe
Shubham Prasad <shubham@totalcloud.io>
2021-04-07 14:42:03 +0530

    testing out cicd pipeline

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 pm2 stop /home/ubuntu/nodejs/ecosystem.config.js || echo "failed to stop the server, skipping ..."
+pm2 delete /home/ubuntu/nodejs/ecosystem.config.js || echo "failed to delete the pm2 process, skipping ..."
