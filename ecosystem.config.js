0bdd23bfdfd6995dbcf903bc923ed51531b87e33
Shubham Prasad <shubham@totalcloud.io>
2021-04-07 14:33:33 +0530

    use index.js to start the server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: "sample-web-app",
-    script: __dirname + "/bin/www",
+    script: __dirname + "/index.js",
     env: {
       NODE_ENV: "development",
     }
